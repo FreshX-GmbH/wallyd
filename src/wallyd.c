a3dc50ec2a20687a1d9579d550bedf9d5f7c67fe
Kai Mosebach <kai@freshx.de>
2016-10-16 10:46:55 +0200

    fix memory hole (free uvthread on exit)

--- src/wallyd.c
+++ src/wallyd.c
@@ -96,6 +96,10 @@ int main(int argc, char *argv[])
 
    uv_loop_close(&loop);
    duk_destroy_heap(ph->ctx);
+   void *pret;
+   if(pthread_join(uv_thr,&pret) == 0){
+      free(uv_thr);
+   }
 }
 
 void allocBuffer(uv_handle_t* handle, size_t size, uv_buf_t* buf) {
