7c226421e3ebd15b37e900efaa57ea26f777aba6
Kai Mosebach <kai@freshx.de>
2017-02-02 10:32:18 +0100

    add client3.js

---
+++ etc/wallyd.d/tests/client3.js
@@ -0,0 +1,27 @@
+nucleus.dofile('modules/bootstrap.js');
+var request = nucleus.dofile('modules/request.js').request;
+var log = nucleus.dofile('modules/log.js');
+
+function getImage(url){
+    log.debug('Requesting url ',url);
+    request(url, function(err,header,body){
+        if(err) {
+           log.error('Could not connect to server : '+err);
+           return(err);
+        }
+        try {
+           //var response = JSON.parse(body);
+            log.info('H:',header);
+            log.info('B:',body);
+        } catch(e) {
+           log.error('Could not decode server response : ',e,body);
+        }
+        log.info('request done.');
+    });
+    log.info('getImage done.');
+}
+
+log.info(request);
+getImage('http://127.0.0.1:1112/wally1920.png');
+
+nucleus.uv.run();
